314311a9b698daf33e02df764361bddc3c9066b7
antonio carè <care.antonio17@gmail.com>
2025-03-18 02:14:47 +0100

    Aggiunto chat.jsx

--- esercizi-react/src/App.jsx
+++ esercizi-react/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Chat from "./components/Chat";
 import Clock from "./components/Clock";
 import Counter from "./components/Counter";
 
 function App() {
   return (
     <>
+      {/* <Chat></Chat> */}
       <Counter initialValue={0} incrementAmount={1}></Counter>
       <Clock></Clock>
     </>
