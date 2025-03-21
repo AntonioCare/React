6a7f453cccbe8c773210b55fd9e7ab6a372a098a
antonio carè <care.antonio17@gmail.com>
2025-03-21 01:40:50 +0100

    Svolto Esercizio Lists

--- esercizi-react/src/App.jsx
+++ esercizi-react/src/App.jsx
@@ -1,26 +1,18 @@
 import "./App.css";
-import Risultato from "./components/Risultato";
-import Saluto from "./components/saluto";
+import "./components/Color";
+import Colors from "./components/Colors";
 
 function App() {
-  const nome = "Antonio";
-  const cognome = "Carè";
-  const persona = {
-    nome: "Antonio",
-    cognome: "Carè",
-    eta: 23,
-  };
-  function handleClick() {
-    console.log("Sono stato cliccato");
-  }
   return (
     <>
-      <Risultato operazione={handleClick}></Risultato>
-      {persona.eta > 18 ? (
-        <Saluto nome={persona.nome} cognome={persona.cognome}></Saluto>
-      ) : (
-        <h3>Non sei autorizzato</h3>
-      )}
+      <Colors
+        colors={[
+          { id: 2, name: "Brown" },
+          { id: 3, name: "Purple" },
+          { id: 4, name: "Green" },
+          { id: 5, name: "Black" },
+        ]}
+      ></Colors>
     </>
   );
 }
