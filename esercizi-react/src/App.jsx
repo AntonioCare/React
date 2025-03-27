60bc1a0a8a16359c90769ebd1cf747d88d943af2
antonio carè <care.antonio17@gmail.com>
2025-03-27 23:12:51 +0100

    Svolto Esercizio DataFetch

--- esercizi-react/src/App.jsx
+++ esercizi-react/src/App.jsx
@@ -1,26 +1,10 @@
 import "./App.css";
-import Risultato from "./components/Risultato";
-import Saluto from "./components/saluto";
+import GithubUsers from "./components/GithubUsers";
 
 function App() {
-  const nome = "Antonio";
-  const cognome = "Carè";
-  const persona = {
-    nome: "Antonio",
-    cognome: "Carè",
-    eta: 23,
-  };
-  function handleClick() {
-    console.log("Sono stato cliccato");
-  }
   return (
     <>
-      <Risultato operazione={handleClick}></Risultato>
-      {persona.eta > 18 ? (
-        <Saluto nome={persona.nome} cognome={persona.cognome}></Saluto>
-      ) : (
-        <h3>Non sei autorizzato</h3>
-      )}
+      <GithubUsers></GithubUsers>
     </>
   );
 }
