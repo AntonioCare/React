5f016f3108c02b2236c36a58aa99adf2c1ee7c56
antonio carè <care.antonio17@gmail.com>
2025-04-03 02:08:36 +0200

    Svolto Esercizio SWR

--- esercizi-react/src/App.jsx
+++ esercizi-react/src/App.jsx
@@ -1,65 +1,21 @@
 import "./App.css";
-import useCounter from "./components/useCounter";
-import useGithubUser from "./components/useGithubUser";
-import useLogin from "./components/useLogin";
-import useCurrentLocation from "./components/useCurrentLocation";
 
-function App({ initialValue = 0 }) {
-  const { counter, onIncrement, onDecrement, onReset } =
-    useCounter(initialValue);
-  const {
-    user,
-    onSubmit,
-    username,
-    password,
-    onChangeUser,
-    onChangePass,
-    onResetUs,
-  } = useLogin("");
-  console.log(user);
-  const { data, error, loading } = useGithubUser("AntonioCare");
-  const { location, errors, loadings, getLocation } = useCurrentLocation();
+import useGithubUser from "./components/useGithubUser";
+
+function App() {
+  const { data, error, isLoading, onRefresh } = useGithubUser("Jok3r");
+  console.log(data);
   return (
     <>
-      <h1>{counter}</h1>
-      <button onClick={onIncrement}>Increment Button</button>
-      <button onClick={onDecrement}>Decrement Button</button>
-      <button onClick={onReset}>Reset Button</button>
-      <h1>Form</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          value={username}
-          placeholder="Inserire Nome Utente"
-          onChange={onChangeUser}
-        />
-        <input
-          type="password"
-          value={password}
-          placeholder="Inserire password"
-          onChange={onChangePass}
-        />
-        <button type="submit">Invio</button>
-        <button onClick={onResetUs}>Reset Ricerca</button>
-      </form>
-
-      <div>
-        {loading && <h1>Loading...</h1>}
-        {error && <h1>There has been an error!</h1>}
-        {data && (
-          <>
-            <h1>
-              {data.name}
-              {data.login}
-            </h1>
-            <img src={data.avatar_url} />
-          </>
-        )}
-      </div>
-      {loadings && <h1>Loading...</h1>}
-      {errors && <h1>Found Error : {errors}</h1>}
-      <button onClick={getLocation}>Ottieni posizione </button>
-      {location && <h1>Position : {location}</h1>}
+      {isLoading && <h1>Loading...</h1>}
+      {error && <h1>There has been an error!</h1>}
+      {data && (
+        <>
+          <button onClick={onRefresh}>Refresh</button>
+          <h1>{data.login}</h1>
+          <img src={data.avatar_url} alt="" />
+        </>
+      )}
     </>
   );
 }
--- esercizi-react/src/components/useGithubUser.jsx
+++ esercizi-react/src/components/useGithubUser.jsx
@@ -1,28 +1,12 @@
-import { useEffect, useState } from "react";
+import useSwr from "swr";
 
 export default function useGithubUser(username) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { data, error, isLoading, mutate } = useSwr(
+    `https://api.github.com/users/${username}`
+  );
 
-  useEffect(() => {
-    setLoading(true);
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => {
-        if (response.status !== 200) {
-          setError(new Error("User Not Found!"));
-        }
-        return response.json();
-      })
-      .then((json) => {
-        console.log(json);
-        setLoading(false);
-        setData(json);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-  }, [username]);
-
-  return { data, loading, error };
+  function handleRefreshUsers() {
+    mutate();
+  }
+  return { data, isLoading, error, onRefresh: handleRefreshUsers };
 }
